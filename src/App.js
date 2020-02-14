bdafc8149132349b80597a90563c75f884d80e8f
mike moore <mike.moore1@mercer.com>
2020-02-14 14:16:42 -0500

    turned main app to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import {Component} from "preact";
+//import {Component} from "preact";
 import Overview from "./homeSections/Overview";
 import NavBar from "./navbar";
 import CardSection from "./cardSections";
@@ -14,7 +14,124 @@ import {varFind,attTrue} from "./util/attFinders.js";
 import {greyBG} from "./sharedStyles.scss";
 import Alert from "./Alert";
 import ajaxCall from "./util/ajaxCall.js";
+import {useState,useEffect, useRef} from "preact/hooks";
 
+
+function parseMods() {
+    let mods = [];
+    $("#contentBox #DeltaPlaceHolderMain table .s4-wpcell-plain").each(function(i,e){
+            
+        let title = $(e).find(".js-webpart-titleCell"),
+            data = $(title).attr("title").split("-")[1].split("_");
+        let item = {
+            header: $(title).text(),
+            type: (data[0])? data[0].trim() : null,
+            html: HTMLclean($(e).html()),
+            attributes: (data[1]) ? data[1].split("|").map(e => e.trim()) : [] 
+        }
+      
+        mods.push(item);
+    });
+    return mods; 
+}
+
+export default function () {
+    const [mods] = useState(parseMods());
+
+    const [alert, updateAlert] = useState(null);
+    const realSections = ["Hero", "Overview", "Cards" , "SummaryText", "AccordionHeader", "PageCopy"];
+    let liveSections = mods.filter(e => realSections.indexOf(varFind(e.attributes, "type")) > -1 );
+    let scrollSections = [];
+    scrollSections["navBar"] = useRef(null)
+    liveSections.forEach((e)=> {
+        scrollSections[e.type] = useRef(null)
+    });
+    
+    const scroller = (section) => {
+      
+        let box = $("#s4-workspace"),
+        toMove = (section === "top") ? scrollSections["navBar"].current : scrollSections[section].current;
+        $(box).animate({
+            scrollTop: $(box).scrollTop() + ($(toMove).offset().top - $(box).offset().top )
+        })
+    }
+    const clickScroll = (e) => {
+        e.preventDefault();
+        scroller(e.currentTarget.getAttribute("href").replace("#",""))
+    }
+    useEffect(()=>{
+        if(PRODUCTION_BUILD) {
+            $("#contentRow").remove();
+        }
+        $("#contentRow").remove();
+        
+        var l = location.hash.replace("#","");
+        if(Object.keys(scrollSections).indexOf(l) > -1) {
+            scroller(l)
+        }    
+        const alertData = (d) => {
+            if(!d.results.length) {
+                return ; 
+            }
+            let item = d.results[0];
+            if(localStorage.getItem("dismissed_alert_"+item.ID) === "yes") {
+                return ; 
+            }
+            updateAlert(item)
+            
+            $(global.alertCloser).on("alert_closed",() => {
+                updateAlert(null);
+            })
+        }
+        if(ALERT_LIST) {
+            ajaxCall(`${SITE_DOMAIN}/_api/web/lists/GetByTitle('${ALERT_LIST}')/items?$orderby=Created desc&$filter=((Expiration_x0020_Date ge datetime'${new Date().toISOString()}') and (Hidden ne 1))`, alertData);
+            
+        }
+    },[])
+   
+    let cardSections = liveSections.map((e,i) =>{
+        let type = varFind(e.attributes, "type");
+        if(realSections.indexOf(type) < 0) {
+            return; 
+        }
+        let chi = null;
+        
+        switch(type) {
+            case "Hero":
+                chi =  <Hero mod={e}  alert={(alert && i === 0)? alert : null} clickScroll={clickScroll} />
+                break;
+            case "Overview":
+                chi = <Overview mod={e} order={i} scroller={scroller} clickScroll={clickScroll} />
+                break; 
+            case "Cards":
+                chi = <CardSection mod={e} order={i} clickScroll={clickScroll} />
+                break;
+            case "SummaryText":
+                chi = <TopText mod={e} allMods={mods} order={i} clickScroll={clickScroll} />
+                break;
+            case "AccordionHeader":
+                chi = <Accordion mod={e} order={i} allMods={mods} clickScroll={clickScroll} /> 
+                break;
+            case "PageCopy" : 
+                chi = <PageCopy mod={e} />
+        }
+        if(!chi) {
+            return;
+        }
+
+        return <div class={(attTrue(e.attributes, "greyBG") ? greyBG : null)} id={e.type} ref={scrollSections[e.type]}>{chi}</div>
+
+    });
+    
+    return <div class={style.globalstrategyapp}>
+            
+            <div id={"navBar"} ref={scrollSections["navBar"]}><NavBar clickScroll={clickScroll} mods={mods} /></div>
+            {(alert)? <Alert alert={alert} onHero={(varFind(liveSections[0].attributes, "type") == "Hero")}/>: null}
+            {cardSections}   
+            <Footer clickScroll={clickScroll} />
+    </div>
+}
+/*
 export default class App extends Component {
     constructor(props) {
         super();
@@ -148,3 +265,4 @@ export default class App extends Component {
         </div>
     }
 } 
+*/
